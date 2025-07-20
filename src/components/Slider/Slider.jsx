50ea540e078c2f5632465cd0910dfcff76ac3627
anurag-10-git <anuragmohan2022@gmail.com>
2025-07-21 03:50:08 +0530

    favicons and case study improvement

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -15,6 +15,14 @@ const Slider = () => {
     }
   }, []);
 
+  const shouldShowCaseStudy = (index) => {
+    return index === 0 || (index + 1) % 4 === 0;
+  };
+
+  const handleCaseStudyClick = () => {
+    window.open('', '_blank');
+  };
+
   return (
     <div className={classes.sponsors}>
       <div className="horizontal-gradient-line"></div>
@@ -26,11 +34,27 @@ const Slider = () => {
           {sponsors.map((e, i) => (
             <li key={`original-${i}`} className={classes["img-container"]}>
               <img src={e} alt={`sponsor-${i}`} />
+              {shouldShowCaseStudy(i) && (
+                <button
+                  className={classes["case-study-btn"]}
+                  onClick={handleCaseStudyClick}
+                >
+                  CASE STUDY
+                </button>
+              )}
             </li>
           ))}
           {sponsors.map((e, i) => (
             <li key={`duplicate-${i}`} className={classes["img-container"]}>
               <img src={e} alt={`sponsor-duplicate-${i}`} />
+              {shouldShowCaseStudy(i) && (
+                <button
+                  className={classes["case-study-btn"]}
+                  onClick={handleCaseStudyClick}
+                >
+                  CASE STUDY
+                </button>
+              )}
             </li>
           ))}
         </ul>
